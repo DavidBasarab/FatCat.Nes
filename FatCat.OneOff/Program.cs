fc071ccb3697e27e2cb9974aaba644663f3ac8ea
David Basarab <dbasarab617@gmail.com>
2019-10-20 21:36:24 -0400

    Playing with bit shifting on C#

--- FatCat.OneOff/Program.cs
+++ FatCat.OneOff/Program.cs
@@ -14,8 +14,20 @@ namespace FatCat.OneOff
 			
 			Log.Info("Going to Play around with some concepts that I will need for the 6502");
 
+			var someAddress = 0x0103;
+			
+			PrintAddress(someAddress);
+			
+			Log.Info("Left Shift");
+			PrintAddress(someAddress << 2);
+			
+			Log.Info("Right Shift");
+			PrintAddress(someAddress >> 2);
+
 			//WaitForExit();
 		}
+
+		private static void PrintAddress(int address) => Log.Info($"Int := {address}  | Hex := {address:x8} | Binary := {Convert.ToString(address, 2)}");
 
 		private static void OnCancel(object sender, ConsoleCancelEventArgs e)
 		{
