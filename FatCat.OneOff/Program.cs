ac6b4a1bd77c6977246ecdf732cdd7dd4b9d1c95
David Basarab <dbasarab617@gmail.com>
2019-10-20 22:04:30 -0400

    more playing

--- FatCat.OneOff/Program.cs
+++ FatCat.OneOff/Program.cs
@@ -12,28 +12,54 @@ namespace FatCat.OneOff
 		{
 			Console.CancelKeyPress += OnCancel;
 			
-			Log.Info("Going to Play around with some concepts that I will need for the 6502");
+			var random = new Random();
 
-			var someAddress = 0x0103;
-			
-			PrintAddress(someAddress);
-			
-			Log.Info("Left Shift");
-			PrintAddress(someAddress << 2);
-			
-			Log.Info("Right Shift");
-			PrintAddress(someAddress >> 2);
+			for (var i = 0; i < 12; i++)
+			{
+				var address = random.Next(0XFFFF);
+				
+				PrintAddress(address);
+			}
+
+			//PlayingWithBitShifting();
 
 			//WaitForExit();
 		}
-
-		private static void PrintAddress(int address) => Log.Info($"Int := {address}  | Hex := {address:x8} | Binary := {Convert.ToString(address, 2)}");
 
 		private static void OnCancel(object sender, ConsoleCancelEventArgs e)
 		{
 			stopEvent.Set();
 
 			e.Cancel = true;
+		}
+
+		private static void PlayingWithBitShifting()
+		{
+			Log.Info("Going to Play around with some concepts that I will need for the 6502");
+
+			var someAddress = 0x0103;
+
+			PrintAddress(someAddress);
+
+			Log.Info("Left Shift");
+			PrintAddress(someAddress << 2);
+
+			Log.Info("Right Shift");
+			PrintAddress(someAddress >> 2);
+		}
+
+		private static void PrintAddress(int address)
+		{
+			IntPtr temp = IntPtr.Zero;
+			
+			var highAddress = address & 0xFF00;
+			var lowAddress = address & 0x00FF;
+			
+			Log.Info(new string('-', 150));
+			
+			Log.Info($"Int := {address}  | Hex := {address:x8} | Binary := {Convert.ToString(address, 2)}");
+			Log.Info($"    ==> HighAddress | Int := {highAddress}  | Hex := {highAddress:x8} | Binary := {Convert.ToString(highAddress, 2)}");
+			Log.Info($"    ==> LowAddress  | Int := {lowAddress}  | Hex := {lowAddress:x8} | Binary := {Convert.ToString(lowAddress, 2)}");
 		}
 
 		private static void WaitForExit()
