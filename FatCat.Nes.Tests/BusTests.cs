6ab943bbd1dba7400184727c897e2ac327b0c03f
David Basarab <dbasarab617@gmail.com>
2019-10-26 15:56:22 -0400

    Added RAM at 64k

--- FatCat.Nes.Tests/BusTests.cs
+++ FatCat.Nes.Tests/BusTests.cs
@@ -1,7 +1,15 @@
+using FluentAssertions;
+using Xunit;
+
 namespace FatCat.Nes.Tests
 {
 	public class BusTests
 	{
-		
+		private Bus bus;
+
+		public BusTests() => bus = new Bus();
+
+		[Fact]
+		public void RamLengthWillBe65K() => bus.Ram.Length.Should().Be(64 * 1024);
 	}
 }
--- FatCat.Nes/Bus.cs
+++ FatCat.Nes/Bus.cs
@@ -1,7 +1,9 @@
+using System.Collections.Generic;
+
 namespace FatCat.Nes
 {
 	public class Bus
 	{
-		
+		internal int[] Ram { get; private set; } = new int[64 * 1024];
 	}
 }
