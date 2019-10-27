a92dcf0c1c7484bb48c8eae81ad33dd1e3ce8446
David Basarab <dbasarab617@gmail.com>
2019-10-27 12:34:46 -0400

    Getting ready for CPU tests

--- FatCat.Nes.Tests/CpuTests/CpuReset.cs
+++ FatCat.Nes.Tests/CpuTests/CpuReset.cs
@@ -4,8 +4,12 @@ namespace FatCat.Nes.Tests.CpuTests
 	{
 		public CpuReset()
 		{
-			
-			
+			cpu.Accumulator = 0x78;
+			cpu.XRegister = 0x12;
+			cpu.YRegister = 0x56;
+			cpu.StackPointer = 0xF3;
+			cpu.StatusRegister = CpuFlag.Break | CpuFlag.Overflow | CpuFlag.CarryBit;
+			cpu.ProgramCounter = 0x7834;
 		}
 	}
 }
