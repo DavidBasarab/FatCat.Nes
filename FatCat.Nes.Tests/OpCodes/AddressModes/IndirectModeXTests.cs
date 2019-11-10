ae65892aedbff55f14787b383009b2032c009d81
David Basarab <dbasarab617@gmail.com>
2019-11-10 17:26:46 -0500

    Some test refacotring

--- FatCat.Nes.Tests/OpCodes/AddressModes/IndirectModeXTests.cs
+++ FatCat.Nes.Tests/OpCodes/AddressModes/IndirectModeXTests.cs
@@ -8,11 +8,16 @@ namespace FatCat.Nes.Tests.OpCodes.AddressModes
 	public class IndirectModeXTests : AddressModeTests
 	{
 		private const byte HighAddressValue = 0x19;
+		
 		private const byte InitialReadValue = 0xe2;
 
 		private const byte LowAddressValue = 0x09;
 
 		private const byte XRegister = 0xd1;
+		
+		private readonly ushort HighLocation = (InitialReadValue + XRegister + 1) & 0x00ff;
+		
+		private readonly ushort LowLocation = (InitialReadValue + XRegister) & 0x00ff;
 
 		protected override int ExpectedCycles => 0;
 
@@ -36,23 +41,19 @@ namespace FatCat.Nes.Tests.OpCodes.AddressModes
 		}
 
 		[Fact]
+		public void WillReadTheHighAddressUsingTheXRegister()
+		{
+			addressMode.Run();
+
+			A.CallTo(() => cpu.Read(HighLocation)).MustHaveHappened();
+		}
+
+		[Fact]
 		public void WillReadTheLowAddressUsingTheXRegister()
 		{
 			addressMode.Run();
 
-			ushort expectedRead = (InitialReadValue + XRegister) & 0x00ff;
-
-			A.CallTo(() => cpu.Read(expectedRead)).MustHaveHappened();
-		}
-
-		[Fact]
-		public void WillReadTheHighAddressUsingTheXRegister()
-		{
-			addressMode.Run(); 
-			
-			ushort expectedRead = (InitialReadValue + XRegister + 1) & 0x00ff;
-
-			A.CallTo(() => cpu.Read(expectedRead)).MustHaveHappened();
+			A.CallTo(() => cpu.Read(LowLocation)).MustHaveHappened();
 		}
 
 		[Fact]
