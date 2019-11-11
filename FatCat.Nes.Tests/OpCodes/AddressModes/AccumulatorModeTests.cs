1f3e5f4217392ed7e4ce8e9d6343330e3648b9e0
David Basarab <dbasarab617@gmail.com>
2019-11-10 21:40:34 -0500

    Made accumlator mode act just as implied mode

--- FatCat.Nes.Tests/OpCodes/AddressModes/AccumulatorModeTests.cs
+++ FatCat.Nes.Tests/OpCodes/AddressModes/AccumulatorModeTests.cs
@@ -1,13 +1,30 @@
 using FatCat.Nes.OpCodes.AddressingModes;
+using FluentAssertions;
+using Xunit;
 
 namespace FatCat.Nes.Tests.OpCodes.AddressModes
 {
 	public class AccumulatorModeTests : AddressModeTests
 	{
+		private const int AccumulatorValue = 0x15;
+
 		protected override int ExpectedCycles => 0;
 
 		protected override string ExpectedName => "Accumulator";
 
-		public AccumulatorModeTests() => addressMode = new AccumulatorMode(cpu);
+		public AccumulatorModeTests()
+		{
+			cpu.Accumulator = AccumulatorValue;
+
+			addressMode = new AccumulatorMode(cpu);
+		}
+
+		[Fact]
+		public void WillSetFetchedValueFromTheAccumulator()
+		{
+			addressMode.Run();
+
+			cpu.Fetched.Should().Be(AccumulatorValue);
+		}
 	}
 }
--- FatCat.Nes/OpCodes/AddressingModes/AccumulatorMode.cs
+++ FatCat.Nes/OpCodes/AddressingModes/AccumulatorMode.cs
@@ -1,15 +1,9 @@
 namespace FatCat.Nes.OpCodes.AddressingModes
 {
-	public class AccumulatorMode : AddressMode
+	public class AccumulatorMode : ImpliedAddressMode
 	{
 		public override string Name => "Accumulator";
 
 		public AccumulatorMode(ICpu cpu) : base(cpu) { }
-
-		/// <summary>
-		/// Do nothing this is because there are some opcodes that just use what is loaded in the accumulator
-		/// </summary>
-		/// <returns></returns>
-		public override int Run() => 0;
 	}
 }
