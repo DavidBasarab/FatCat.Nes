c2732c0012ad5a6fbcc8ef1c9a310570b3023348
David Basarab <dbasarab617@gmail.com>
2019-11-16 19:32:08 -0500

    Will read the datat from the absolute address

--- FatCat.Nes.Tests/OpCodes/AddWithCarryTests.cs
+++ FatCat.Nes.Tests/OpCodes/AddWithCarryTests.cs
@@ -1,21 +1,37 @@
 using FakeItEasy;
 using FatCat.Nes.OpCodes;
 using FatCat.Nes.OpCodes.AddressingModes;
+using Xunit;
 
 namespace FatCat.Nes.Tests.OpCodes
 {
 	public class AddWithCarryTests
 	{
+		private const ushort AbsoluteAddress = 0x2807;
+		private const byte FetchedData = 0x13;
 		private readonly IAddressMode addressMode;
 		private readonly ICpu cpu;
-		private AddWithCarry opCode;
+		private readonly AddWithCarry opCode;
 
 		public AddWithCarryTests()
 		{
 			cpu = A.Fake<ICpu>();
+
+			cpu.AbsoluteAddress = AbsoluteAddress;
+
+			A.CallTo(() => cpu.Read(AbsoluteAddress)).Returns(FetchedData);
+
 			addressMode = A.Fake<IAddressMode>();
 
 			opCode = new AddWithCarry(cpu, addressMode);
+		}
+
+		[Fact]
+		public void WillReadFromTheAbsoluteAddress()
+		{
+			opCode.Execute();
+
+			A.CallTo(() => cpu.Read(AbsoluteAddress)).MustHaveHappened();
 		}
 	}
 }
--- FatCat.Nes/OpCodes/AddWithCarry.cs
+++ FatCat.Nes/OpCodes/AddWithCarry.cs
@@ -7,6 +7,11 @@ namespace FatCat.Nes.OpCodes
 	{
 		public AddWithCarry(ICpu cpu, IAddressMode addressMode) : base(cpu, addressMode) { }
 
-		public override int Execute() => throw new NotImplementedException();
+		public override int Execute()
+		{
+			var fetchedData = cpu.Read(cpu.AbsoluteAddress);
+			
+			return -1;
+		}
 	}
 }
