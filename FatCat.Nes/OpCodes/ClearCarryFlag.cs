1e56629079bf1931c02335129195fd2ca1af8c11
David Basarab <dbasarab617@gmail.com>
2019-11-24 11:50:16 -0500

    Some abstraction

--- FatCat.Nes/OpCodes/ClearCarryFlag.cs
+++ FatCat.Nes/OpCodes/ClearCarryFlag.cs
@@ -2,17 +2,12 @@ using FatCat.Nes.OpCodes.AddressingModes;
 
 namespace FatCat.Nes.OpCodes
 {
-	public class ClearCarryFlag : OpCode
+	public class ClearCarryFlag : ClearFlagOpCode
 	{
 		public override string Name => "CLC";
 
-		public ClearCarryFlag(ICpu cpu, IAddressMode addressMode) : base(cpu, addressMode) { }
+		protected override CpuFlag Flag => CpuFlag.CarryBit;
 
-		public override int Execute()
-		{
-			cpu.RemoveFlag(CpuFlag.CarryBit);
-			
-			return 0;
-		}
+		public ClearCarryFlag(ICpu cpu, IAddressMode addressMode) : base(cpu, addressMode) { }
 	}
 }
---
+++ FatCat.Nes/OpCodes/ClearFlagOpCode.cs
@@ -0,0 +1,18 @@
+using FatCat.Nes.OpCodes.AddressingModes;
+
+namespace FatCat.Nes.OpCodes
+{
+	public abstract class ClearFlagOpCode : OpCode
+	{
+		protected abstract CpuFlag Flag { get; }
+
+		protected ClearFlagOpCode(ICpu cpu, IAddressMode addressMode) : base(cpu, addressMode) { }
+
+		public override int Execute()
+		{
+			cpu.RemoveFlag(Flag);
+
+			return 0;
+		}
+	}
+}
