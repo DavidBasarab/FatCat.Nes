2642ec06f2ccd7d8179fd402264e1a6d650ed2f6
David Basarab <dbasarab617@gmail.com>
2019-11-24 11:38:36 -0500

    Branch if overflow set completed

---
+++ FatCat.Nes.Tests/OpCodes/Branching/BranchIfOverFlowSetTests.cs
@@ -0,0 +1,17 @@
+using FatCat.Nes.OpCodes;
+using JetBrains.Annotations;
+
+namespace FatCat.Nes.Tests.OpCodes.Branching
+{
+	[UsedImplicitly]
+	public class BranchIfOverFlowSetTests : BranchTests
+	{
+		protected override string ExpectedName => "BVS";
+
+		protected override CpuFlag Flag => CpuFlag.Overflow;
+
+		protected override bool FlagState => true;
+
+		public BranchIfOverFlowSetTests() => opCode = new BranchIfOverFlowSet(cpu, addressMode);
+	}
+}
---
+++ FatCat.Nes/OpCodes/BranchIfOverFlowSet.cs
@@ -0,0 +1,15 @@
+using FatCat.Nes.OpCodes.AddressingModes;
+
+namespace FatCat.Nes.OpCodes
+{
+	public class BranchIfOverFlowSet : BranchOpCode
+	{
+		public override string Name => "BVS";
+
+		protected override CpuFlag Flag => CpuFlag.Overflow;
+
+		protected override bool FlagState => true;
+
+		public BranchIfOverFlowSet(ICpu cpu, IAddressMode addressMode) : base(cpu, addressMode) { }
+	}
+}
