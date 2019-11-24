2c4f34d8dcbe94d229b60eadb1bb88d73ce0b56c
David Basarab <dbasarab617@gmail.com>
2019-11-24 11:33:59 -0500

    branch if overflow clear test

---
+++ FatCat.Nes.Tests/OpCodes/Branching/BranchIfOverflowClearTests.cs
@@ -0,0 +1,17 @@
+using FatCat.Nes.OpCodes;
+using JetBrains.Annotations;
+
+namespace FatCat.Nes.Tests.OpCodes.Branching
+{
+	[UsedImplicitly]
+	public class BranchIfOverflowClearTests : BranchTests
+	{
+		protected override string ExpectedName => "BVC";
+
+		protected override CpuFlag Flag => CpuFlag.Overflow;
+
+		protected override bool FlagState => false;
+
+		public BranchIfOverflowClearTests() => opCode = new BranchIfOverflowClear(cpu, addressMode);
+	}
+}
---
+++ FatCat.Nes/OpCodes/BranchIfOverflowClear.cs
@@ -0,0 +1,15 @@
+using FatCat.Nes.OpCodes.AddressingModes;
+
+namespace FatCat.Nes.OpCodes
+{
+	public class BranchIfOverflowClear : BranchOpCode
+	{
+		public override string Name => "BVC";
+
+		protected override CpuFlag Flag => CpuFlag.Overflow;
+
+		protected override bool FlagState => false;
+
+		public BranchIfOverflowClear(ICpu cpu, IAddressMode addressMode) : base(cpu, addressMode) { }
+	}
+}
