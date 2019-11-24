3cc6079db9f18e598981a94f2ee9a3097b734bcd
David Basarab <dbasarab617@gmail.com>
2019-11-24 10:53:50 -0500

    Writing program counter to the stack

--- FatCat.Nes.Tests/OpCodes/BreakTests.cs
+++ FatCat.Nes.Tests/OpCodes/BreakTests.cs
@@ -6,9 +6,18 @@ namespace FatCat.Nes.Tests.OpCodes
 {
 	public class BreakTests : OpCodeTest
 	{
+		private const ushort ProgramCounter = 0x2817;
+		private const byte StackPointer = 0x32;
+
 		protected override string ExpectedName => "BRK";
 
-		public BreakTests() => opCode = new Break(cpu, addressMode);
+		public BreakTests()
+		{
+			opCode = new Break(cpu, addressMode);
+
+			cpu.ProgramCounter = ProgramCounter;
+			cpu.StackPointer = StackPointer;
+		}
 
 		[Fact]
 		public void WillSetTheInterruptFlag()
@@ -16,6 +25,30 @@ namespace FatCat.Nes.Tests.OpCodes
 			opCode.Execute();
 
 			A.CallTo(() => cpu.SetFlag(CpuFlag.DisableInterrupts)).MustHaveHappened();
+		}
+
+		[Fact]
+		public void WillWriteTheHighMemoryProgramCounter()
+		{
+			opCode.Execute();
+
+			byte expectedProgramCounter = ((ProgramCounter + 1) >> 8) & 0x00ff;
+
+			ushort writeAddress = StackPointer + 0x0100;
+
+			A.CallTo(() => cpu.Write(writeAddress, expectedProgramCounter)).MustHaveHappened();
+		}
+
+		[Fact]
+		public void WIllWriteTheLowMemoryOfTheProgramCounter()
+		{
+			opCode.Execute();
+
+			byte expectedProgramCounter = (ProgramCounter + 1) & 0x00ff;
+
+			ushort writeAddress = StackPointer - 1 + 0x0100;
+
+			A.CallTo(() => cpu.Write(writeAddress, expectedProgramCounter)).MustHaveHappened();
 		}
 	}
 }
--- FatCat.Nes/OpCodes/Break.cs
+++ FatCat.Nes/OpCodes/Break.cs
@@ -10,9 +10,21 @@ namespace FatCat.Nes.OpCodes
 
 		public override int Execute()
 		{
+			cpu.ProgramCounter++;
+
 			cpu.SetFlag(CpuFlag.DisableInterrupts);
-			
+
+			WriteToStack((byte)((cpu.ProgramCounter >> 8) & 0x00ff));
+			WriteToStack((byte)(cpu.ProgramCounter & 0x00ff));
+
 			return -1;
+		}
+
+		private void WriteToStack(byte data)
+		{
+			cpu.Write((ushort)(0x0100 + cpu.StackPointer), data);
+
+			cpu.StackPointer--;
 		}
 	}
 }
