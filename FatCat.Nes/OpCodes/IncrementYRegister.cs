7ab4c1c41e901aae14244f0cd522cb53b62f2cfe
David Basarab <dbasarab617@gmail.com>
2019-11-25 15:57:55 -0500

    Increment Y value

---
+++ FatCat.Nes/OpCodes/IncrementYRegister.cs
@@ -0,0 +1,22 @@
+using System;
+using FatCat.Nes.OpCodes.AddressingModes;
+
+namespace FatCat.Nes.OpCodes
+{
+	public class IncrementYRegister : OpCode
+	{
+		public override string Name => "INY";
+
+		public IncrementYRegister(ICpu cpu, IAddressMode addressMode) : base(cpu, addressMode) { }
+
+		public override int Execute()
+		{
+			cpu.YRegister++;
+
+			ApplyFlag(cpu.YRegister.IsZero(), CpuFlag.Zero);
+			ApplyFlag(cpu.YRegister.IsNegative(), CpuFlag.Negative);
+
+			return 0;
+		}
+	}
+}
