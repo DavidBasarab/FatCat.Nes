4052137598291283602c20a608c8f9e7d9e79e5b
David Basarab <dbasarab617@gmail.com>
2019-11-25 13:26:59 -0500

    Will set the zero and negative flags

--- FatCat.Nes/OpCodes/ExclusiveOr.cs
+++ FatCat.Nes/OpCodes/ExclusiveOr.cs
@@ -4,16 +4,19 @@ namespace FatCat.Nes.OpCodes
 {
 	public class ExclusiveOr : OpCode
 	{
-		public ExclusiveOr(ICpu cpu, IAddressMode addressMode) : base(cpu, addressMode) { }
-
 		public override string Name => "EOR";
+
+		public ExclusiveOr(ICpu cpu, IAddressMode addressMode) : base(cpu, addressMode) { }
 
 		public override int Execute()
 		{
 			Fetch();
 
 			cpu.Accumulator = (byte)(cpu.Accumulator ^ fetched);
-			
+
+			ApplyFlag(cpu.Accumulator.IsZero(), CpuFlag.Zero);
+			ApplyFlag(cpu.Accumulator.IsNegative(), CpuFlag.Negative);
+
 			return -1;
 		}
 	}
