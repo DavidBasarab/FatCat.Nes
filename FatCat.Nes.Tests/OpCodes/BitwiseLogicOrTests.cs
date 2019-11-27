22a6b79adeec704cdc793a8a47e179e9e802b47d
David Basarab <dbasarab617@gmail.com>
2019-11-26 21:28:13 -0500

    Will apply zero flag

--- FatCat.Nes.Tests/OpCodes/BitwiseLogicOrTests.cs
+++ FatCat.Nes.Tests/OpCodes/BitwiseLogicOrTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using FakeItEasy;
 using FatCat.Nes.OpCodes;
 using FluentAssertions;
@@ -7,9 +8,40 @@ namespace FatCat.Nes.Tests.OpCodes
 {
 	public class BitwiseLogicOrTests : OpCodeTest
 	{
+		public static IEnumerable<object[]> ZeroData
+		{
+			get
+			{
+				yield return new object[]
+							{
+								0b_1111_1111, // accumulator
+								0b_1111_1111, // fetched
+								false         // flag set
+							};
+
+				yield return new object[]
+							{
+								0b_0000_0000, // accumulator
+								0b_0000_0000, // fetched
+								true          // flag set
+							};
+
+				yield return new object[]
+							{
+								0b_1010_1010, // accumulator
+								0b_0111_0101, // fetched
+								false         // flag set
+							};
+			}
+		}
+
 		protected override string ExpectedName => "ORA";
 
 		public BitwiseLogicOrTests() => opCode = new BitwiseLogicOr(cpu, addressMode);
+
+		[Theory]
+		[MemberData(nameof(ZeroData), MemberType = typeof(BitwiseLogicOrTests))]
+		public void WillApplyZeroFlag(byte accumulator, byte fetched, bool flagSet) => RunApplyFlagTest(accumulator, fetched, flagSet, CpuFlag.Zero);
 
 		[Fact]
 		public void WillFetchFromTheAddressMode()
@@ -27,6 +59,26 @@ namespace FatCat.Nes.Tests.OpCodes
 			byte expectedValue = Accumulator | FetchedData;
 
 			cpu.Accumulator.Should().Be(expectedValue);
+		}
+
+		private void RunApplyFlagTest(byte accumulator, byte fetched, bool flagSet, CpuFlag flag)
+		{
+			cpu.Accumulator = accumulator;
+
+			A.CallTo(() => addressMode.Fetch()).Returns(fetched);
+
+			opCode.Execute();
+
+			if (flagSet)
+			{
+				A.CallTo(() => cpu.SetFlag(flag)).MustHaveHappened();
+				A.CallTo(() => cpu.RemoveFlag(flag)).MustNotHaveHappened();
+			}
+			else
+			{
+				A.CallTo(() => cpu.RemoveFlag(flag)).MustHaveHappened();
+				A.CallTo(() => cpu.SetFlag(flag)).MustNotHaveHappened();
+			}
 		}
 	}
 }
--- FatCat.Nes/OpCodes/BitwiseLogicOr.cs
+++ FatCat.Nes/OpCodes/BitwiseLogicOr.cs
@@ -4,16 +4,18 @@ namespace FatCat.Nes.OpCodes
 {
 	public class BitwiseLogicOr : OpCode
 	{
-		public BitwiseLogicOr(ICpu cpu, IAddressMode addressMode) : base(cpu, addressMode) { }
-
 		public override string Name => "ORA";
+
+		public BitwiseLogicOr(ICpu cpu, IAddressMode addressMode) : base(cpu, addressMode) { }
 
 		public override int Execute()
 		{
 			Fetch();
 
 			cpu.Accumulator = (byte)(cpu.Accumulator | fetched);
-			
+
+			ApplyFlag(CpuFlag.Zero, cpu.Accumulator.IsZero());
+
 			return -1;
 		}
 	}
